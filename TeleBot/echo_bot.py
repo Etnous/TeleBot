cf2c3e0416f281ef84caba20651f89855f5c0e0c
Etnous <40648517+Etnous@users.noreply.github.com>
2020-03-25 14:17:50 +0800

    Update and rename echo_bot21.py to echo_bot.py

---
+++ TeleBot/echo_bot.py
@@ -0,0 +1,19 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "大噶猴啊！")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "有什么好帮你的？？？？")
+    
+if __name__ == '__main__':
+    bot.polling()
